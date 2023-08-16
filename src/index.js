5cc0e5e059a16ad59a07794feed613edee92b00c
Brian Scherner <brianscherner@gmail.com>
2023-08-16 16:03:33 -0700

    Add div class for keywordInput and group radio buttons. Update getGif functionality, make createImage function and update printElements function. Create checkRadioButtons function and update form submission. Add CSS rule.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-function getGif(keyword) {
+function getGif(keyword, searchType) {
   let request = new XMLHttpRequest();
-  const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${process.env.API_KEY}`;
+  let url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${process.env.API_KEY}`;
+  if (searchType === "trending") {
+    url = `http://api.giphy.com/v1/gifs/trending?&api_key=${process.env.API_KEY}`;
+  } else if (searchType === "random") {
+    url = `http://api.giphy.com/v1/gifs/random?&api_key=${process.env.API_KEY}`;
+  }
 
-
-  request.addEventListener("loadend", function() {
+  request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       printElements(response, keyword);
@@ -24,24 +28,50 @@ function printError(apiResponse, keyword) {
   document.querySelector("#showGifs").innerHTML = `There was an error accessing gifs for ${keyword}: ${apiResponse.meta.status} ${apiResponse.meta.msg}`;
 }
 
+function createImage(url) {
+  let gifsDiv = document.querySelector("div#showGifs")
+  let img = document.createElement("img");
+  img.src = url;
+  img.height = "300";
+  img.width = "400";
+  gifsDiv.append(img);
+}
+
 function printElements(apiResponse) {
   let gifsDiv = document.querySelector("div#showGifs")
   gifsDiv.innerHTML = null;
-  for (let index = 0; index < 10; index++) {
-    let img = document.createElement("img");
-    img.src = apiResponse.data[index].images.original.url;
-    img.height = "300";
-    img.width = "400";
-    gifsDiv.append(img);
+  if (Array.isArray(apiResponse.data)) {
+    for (let index = 0; index < 12; index++) {
+      createImage(apiResponse.data[index].images.original.url);
+    }
+  } else {
+    createImage(apiResponse.data.images.original.url);
+  }
+}
+
+function checkRadioButtons(event) {
+  event.preventDefault();
+  const radioButtonValue = document.querySelector("input[name='search']:checked").value;
+  if (radioButtonValue === "byKeyword") {
+    const keywordInput = document.getElementById("keywordInput");
+    keywordInput.removeAttribute("class", "form-group hidden");
   }
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const keyword = document.querySelector("#keyword").value;
-  getGif(keyword);
+  const trendingOrRandom = document.querySelector("input[name='search']:checked").value;
+  if (trendingOrRandom !== "byKeyword") {
+    console.log(trendingOrRandom);
+    getGif(null, trendingOrRandom);
+  } else {
+    const keyword = document.querySelector("#keyword").value;
+    console.log(keyword);
+    getGif(keyword);
+  }
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  // document.querySelector("#radioButtons").addEventListener("click", checkRadioButtons);
   document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
